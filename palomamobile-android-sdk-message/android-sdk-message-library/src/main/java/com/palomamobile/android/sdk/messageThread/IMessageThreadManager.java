04012ac19a9eed332728d047e76e77a6e788c2c3
Karel Herink <karel.herink@gmail.com>
2015-10-01 10:14:22 +1000

    working on ASDK-71, message threads

--- palomamobile-android-sdk-message/android-sdk-message-library/src/main/java/com/palomamobile/android/sdk/messageThread/IMessageThreadManager.java
+++ palomamobile-android-sdk-message/android-sdk-message-library/src/main/java/com/palomamobile/android/sdk/messageThread/IMessageThreadManager.java
@@ -9,27 +9,95 @@ import com.palomamobile.android.sdk.message.MessageSent;
 public interface IMessageThreadManager extends IServiceManager<IMessageThreadService> {
 
     @Override
-    public IMessageThreadService getService();
+    IMessageThreadService getService();
 
+    /**
+     * A convenience method o create a new {@link JobPostMessageThread} 
+     *
+     * @param newMessageThread
+     * @return newly created job
+     */
     JobPostMessageThread createJobPostMessageThread(NewMessageThread newMessageThread);
 
+    /**
+     * A convenience method o create a new {@link JobGetMessageThread} 
+     *
+     * @param messageThreadId
+     * @return newly created job
+     */
     JobGetMessageThread createJobGetMessageThread(long messageThreadId);
 
+    /**
+     * A convenience method o create a new {@link JobUpdateMessageThread} 
+     *
+     * @param messageThreadId
+     * @param update
+     * @return newly created job
+     */
     JobUpdateMessageThread createJobUpdateMessageThread(long messageThreadId, MessageThreadUpdate update);
 
+    /**
+     * A convenience method o create a new {@link JobDeleteMessageThread} 
+     *
+     * @param messageThreadId
+     * @return newly created job
+     */
     JobDeleteMessageThread createJobDeleteMessageThread(long messageThreadId);
 
+    /**
+     * A convenience method o create a new {@link JobGetMessageThreadMembers} 
+     *
+     * @param messageThreadId
+     * @return newly created job
+     */
     JobGetMessageThreadMembers createJobGetMessageThreadMembers(long messageThreadId);
 
+    /**
+     * A convenience method o create a new {@link JobAddMessageThreadMember} 
+     *
+     * @param messageThreadId
+     * @param userId
+     * @return newly created job
+     */
     JobAddMessageThreadMember createJobAddMessageThreadMember(long messageThreadId, long userId);
 
+    /**
+     * A convenience method o create a new {@link JobDeleteMessageThreadMember} 
+     *
+     * @param messageThreadId
+     * @param userId
+     * @return newly created job
+     */
     JobDeleteMessageThreadMember createJobDeleteMessageThreadMember(long messageThreadId, long userId);
 
+    /**
+     * A convenience method o create a new {@link JobGetMessageThreadMessages} 
+     *
+     * @param messageThreadId
+     * @return newly created job
+     */
     JobGetMessageThreadMessages createJobGetMessageThreadMessages(long messageThreadId);
 
+    /**
+     * A convenience method o create a new {@link JobPostMessageThreadMessage} 
+     *
+     * @param messageThreadId
+     * @param newMessage
+     * @return newly created job
+     */
     JobPostMessageThreadMessage createJobPostMessageThreadMessage(long messageThreadId, MessageSent newMessage);
 
+    /**
+     * A convenience method o create a new {@link JobGetMessageThreads} 
+     *
+     * @return newly created job
+     */
     JobGetMessageThreads createJobGetMessageThreads();
 
+    /**
+     * A convenience method o create a new {@link JobDeleteMessageThreads} 
+     *
+     * @return newly created job
+     */
     JobDeleteMessageThreads createJobDeleteMessageThreads();
 }
--- palomamobile-android-sdk-message/android-sdk-message-library/src/main/java/com/palomamobile/android/sdk/messageThread/IMessageThreadService.java
+++ palomamobile-android-sdk-message/android-sdk-message-library/src/main/java/com/palomamobile/android/sdk/messageThread/IMessageThreadService.java
@@ -28,51 +28,135 @@ import java.util.Map;
 public interface IMessageThreadService {
 
     /**
+     * Create a new named message thread, with an initial set of members.
      *
      * @param requestId
      * @param newMessageThread
-     * @return
+     * @return newly created message thread
      */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @POST("/threads")
     MessageThread postMessageThread(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Body NewMessageThread newMessageThread);
 
+    /**
+     * Read thread attributes for the specified thread id.
+     *
+     * @param requestId
+     * @param threadId
+     * @return message thread
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @GET("/threads/{threadId}")
     MessageThread getMessageThread(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("threadId") long threadId);
 
+    /**
+     * Update the details of an existing thread.
+     *
+     * @param requestId
+     * @param threadId
+     * @param messageThreadUpdate
+     * @return updated message thread
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @PUT("/threads/{threadId}")
     MessageThread updateMessageThread(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("threadId") long threadId, @Body MessageThreadUpdate messageThreadUpdate);
 
+    /**
+     * Delete the thread associated with the specified thread id.
+     * @param requestId
+     * @param threadId
+     * @return nothing
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @DELETE("/threads/{threadId}")
     Void deleteMessageThread(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("threadId") long threadId);
 
+    /**
+     * List all members of the specified thread, including the owner.
+     *
+     * @param requestId
+     * @param threadId
+     * @param options
+     * @param filterQuery
+     * @param sortOrder
+     * @return paginated list of message thread members
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @GET("/threads/{threadId}/members")
     PaginatedResponse<MessageThreadMember> getMessageThreadMembers(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("threadId") long threadId, @QueryMap Map<String, String> options, @Query("where") String filterQuery, @Query("sort") String... sortOrder);
 
+    /**
+     * Add the specified user to the thread as a member.
+     *
+     * @param requestId
+     * @param threadId
+     * @param userId
+     * @param emptyBody
+     * @return added member
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @PUT("/threads/{threadId}/members/{userId}")
     MessageThreadMember addMessageThreadMember(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("threadId") long threadId, @Path("userId") long userId, @Body String emptyBody);
 
+    /**
+     * Removes the specified user from the thread. All their existing received and sent thread messages remain, they just no longer can send or receive any new thread messages.
+     *
+     * @param requestId
+     * @param threadId
+     * @param userId
+     * @return nothing
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @DELETE("/threads/{threadId}/members/{userId}")
     Void deleteMessageThreadMember(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("threadId") long threadId, @Path("userId") long userId);
 
+    /**
+     * List all messages of the specified thread.
+     *
+     * @param requestId
+     * @param threadId
+     * @param options
+     * @param filterQuery
+     * @param sortOrder
+     * @return list of thread messages
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @GET("/threads/{threadId}/messages")
     PaginatedResponse<MessageSent> getMessages(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("threadId") long threadId, @QueryMap Map<String, String> options, @Query("where") String filterQuery, @Query("sort") String... sortOrder);
 
+    /**
+     * Send message to thread and hence to all members of the thread.
+     *
+     * @param requestId
+     * @param threadId
+     * @param message
+     * @return
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @POST("/threads/{threadId}/messages")
     MessageSent postMessage(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("threadId") long threadId, @Body MessageSent message);
 
+    /**
+     * List all the threads the user is a member of.
+     *
+     * @param requestId
+     * @param userId
+     * @param options
+     * @param filterQuery
+     * @param sortOrder
+     * @return
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @GET("/users/{userId}/threads")
     PaginatedResponse<MessageThread> getMessageThreads(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("userId") long userId, @QueryMap Map<String, String> options, @Query("where") String filterQuery, @Query("sort") String... sortOrder);
 
+    /**
+     * Delete all the threads the user is a owner of.
+     *
+     * @param requestId
+     * @param userId
+     * @return
+     */
     @Headers({CustomHeader.HEADER_PALOMA_TARGET_SERVICE_VERSION + ": " + BuildConfig.TARGET_SERVICE_VERSION, CustomHeader.HEADER_PALOMA_SDK_MODULE_VERSION + ": " + BuildConfig.VERSION_NAME})
     @DELETE("/users/{userId}/threads")
     Void deleteMessageThreads(@Header(CustomHeader.HEADER_NAME_PALOMA_REQUEST) String requestId, @Path("userId") long userId);
